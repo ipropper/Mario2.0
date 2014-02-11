d1ac24cbc5878f48f15e39a298f88416687dd0de
Ian Propper <ipropper@umich.edu>
2014-02-11 16:51:31 -0500

    added Jabba

---
+++ Assets/RecieveOn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecieveOn : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject Question;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(TurnStuffOn.On == true)
+		{
+			StartCoroutine("Suprise");
+
+		}
+	}
+	IEnumerator Suprise() 
+	{
+		yield return new WaitForSeconds(3.0f);
+		Instantiate(Question,transform.position,transform.rotation);
+		Destroy(this.gameObject);
+	}
+}
